192de06af483f0225f7ccf2f1bdd72c4e2a01041
zhuravlevvik <viktor.zhuravlev2003@mail.ru>
2025-02-23 13:29:30 +0300

    base datasets classes rework

--- src/data/base_datasets.py
+++ src/data/base_datasets.py
@@ -3,28 +3,63 @@ from torch.utils.data import Dataset
 from transformers import PreTrainedTokenizer
 import torch
 import pandas as pd
-from typing import Tuple, List, Sequence
+from typing import Tuple, List
 import os
+import json
+from abc import ABC, abstractmethod
 
 
-class BaseDataset(Dataset):
+class BaseDataset(Dataset, ABC):
 
     def __init__(
         self,
+        name: str,
         tokenizer: PreTrainedTokenizer,
         data_path: str,
-        prompt: str = "",
+        prompt_config_dir_path: str,
+        prompt: str = None,
         max_seq_length: int = None
     ) -> None:
         super().__init__()
+
+        self.name = name
         self.tokenizer = tokenizer
         self.data_path = data_path
-        self.prompt = prompt
+        self.config_path = prompt_config_dir_path
+
+        if prompt is not None:
+            self.prompt = prompt
+        else:
+            self.prompt = self._get_basic_prompt()
+
+        self._use_prompt_template()
 
         self.df = self._read_data()
         self.max_seq_length = max_seq_length
 
-        self.input_ids, self.attention_mask, self.labels = self._process_data()
+        (
+            self.input_ids,
+            self.attention_mask,
+            self.num_labels,
+        ) = self._process_data()
+
+    def _get_basic_prompt(self) -> str:
+        return self._get_data_from_config("basic_prompts.json")
+
+    @abstractmethod
+    def _get_prompt_template(self) -> str:
+        pass
+
+    def _get_data_from_config(self, config_filename: str, key: str = None):
+        path = os.path.join(self.config_path, config_filename)
+        with open(path, 'r') as f:
+            data = json.load(f)
+        key = key if key is not None else self.name
+        return data[key]
+
+    @abstractmethod
+    def _use_prompt_template(self) -> None:
+        pass
 
     def _read_data(self) -> pd.DataFrame:
         with open(self.data_path, 'rb') as file:
@@ -45,23 +80,12 @@ class BaseDataset(Dataset):
 
         return (input_ids, attention_mask)
 
+    @abstractmethod
     def _process_data(self) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        sentences = self.df['input']
-        labels = self.df['target']
-
-        messages = [self.prompt + sentence + "\nResponse:\n"
-                    for sentence in sentences]
-
-        input_ids, attention_mask = self._tokenize(messages)
-
-        return (
-            input_ids.type(torch.long),
-            attention_mask.type(torch.long),
-            torch.Tensor(labels).type(torch.long)
-        )
+        pass
 
     def __len__(self):
-        return len(self.labels)
+        return len(self.num_labels)
 
     def __getitem__(
         self,
@@ -70,36 +94,171 @@ class BaseDataset(Dataset):
         return (
             self.input_ids[ind],
             self.attention_mask[ind],
-            self.labels[ind]
+            self.num_labels[ind]
         )
 
 
-class BaseNonLabeledDataset(BaseDataset):
+class BaseClassificationDataset(BaseDataset):
 
     def __init__(
         self,
+        name: str,
         tokenizer: PreTrainedTokenizer,
         data_path: str,
-        prompt: str = "",
+        prompt_config_dir_path: str,
+        prompt: str = None,
         max_seq_length: int = None
     ) -> None:
-        super().__init__(tokenizer, data_path, prompt, max_seq_length)
+        self.labels = self._get_labels()
+
+        super().__init__(
+            name=name,
+            tokenizer=tokenizer,
+            data_path=data_path,
+            prompt_config_dir_path=prompt_config_dir_path,
+            prompt=prompt,
+            max_seq_length=max_seq_length
+        )
+
+    def _get_labels(self) -> List[str]:
+        return self._get_data_from_config('labels.json')
+
+    def _get_prompt_template(self) -> str:
+        return self._get_data_from_config(
+            "prompt_templates.json",
+            key='classification'
+        )
+
+    def _use_prompt_template(self) -> None:
+        template = self._get_prompt_template()
+        template = template.replace("<PROMPT>", self.prompt)
+        self.prompt = template.replace("<LABELS>", ','.join(self.labels))
 
     def _process_data(self) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         sentences = self.df['input']
-        targets = list(self.df['target'])
+        labels = list(self.df['target'])
+        numeric_labels = labels_to_numbers(labels, self.labels)
 
-        messages = [self.prompt + sentence + "\nResponse:\n"
-                    for sentence in sentences]
+        inputs = [self.prompt.replace('<INPUT>', sentence)
+                  for sentence in sentences]
 
-        input_ids, attention_mask = self._tokenize(messages)
-        target_ids, _ = self._tokenize(targets)
+        input_ids, attention_mask = self._tokenize(inputs)
 
         return (
             input_ids.type(torch.long),
             attention_mask.type(torch.long),
-            torch.Tensor(target_ids).type(torch.long)
+            torch.Tensor(numeric_labels).type(torch.long)
         )
+
+
+class BaseGenerationDataset(BaseDataset):
+
+    def __init__(
+        self,
+        name: str,
+        tokenizer: PreTrainedTokenizer,
+        data_path: str,
+        prompt_config_dir_path: str,
+        prompt: str = None,
+        max_seq_length: int = None
+    ) -> None:
+        self.response_prefix = self._get_response_prefix()
+
+        super().__init__(
+            name=name,
+            tokenizer=tokenizer,
+            data_path=data_path,
+            prompt_config_dir_path=prompt_config_dir_path,
+            prompt=prompt,
+            max_seq_length=max_seq_length
+        )
+
+    def _get_response_prefix(self) -> str:
+        return self._get_data_from_config('response_prefixes.json')
+
+    def _get_prompt_template(self) -> str:
+        return self._get_data_from_config(
+            "prompt_templates.json",
+            key='generation'
+        )
+
+    def _use_prompt_template(self) -> None:
+        template = self._get_prompt_template()
+        template = template.replace("<PROMPT>", self.prompt)
+        self.prompt = template.replace(
+            "<RESPONSE_PREFIX>",
+            self.response_prefix
+        )
+
+    def _process_data(self) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        sentences = self.df['input']
+        target_outputs = list(self.df['target'])
+
+        inputs = [self.prompt.replace('<INPUT>', sentence)
+                  for sentence in sentences]
+
+        input_ids, attention_mask = self._tokenize(inputs)
+
+        target_ids, _ = self._tokenize(target_outputs)
+
+        return (
+            input_ids.type(torch.long),
+            attention_mask.type(torch.long),
+            target_ids.type(torch.long)
+        )
+
+
+GENERATION_TASKS = set([
+    'todo',
+])
+
+
+class InnerTaskClassificationDataset(BaseClassificationDataset):
+
+    def __init__(
+        self,
+        base_name: str,
+        name: str,
+        tokenizer: PreTrainedTokenizer,
+        data_path: str,
+        prompt: str = None,
+        max_seq_length: int = None,
+    ) -> None:
+        self.base_name = base_name
+        super().__init__(name, tokenizer, data_path, prompt, max_seq_length)
+
+    def _get_data_from_config(self, config_filename: str, key: str = None):
+        path = os.path.join(self.config_path, config_filename)
+        with open(path, 'r') as f:
+            data = json.load(f)
+
+        if key is not None:
+            return data[key]
+        return data[self.base_name][self.name]
+   
+
+class InnerTaskGenerationDataset(BaseGenerationDataset):
+
+    def __init__(
+        self,
+        base_name: str,
+        name: str,
+        tokenizer: PreTrainedTokenizer,
+        data_path: str,
+        prompt: str = None,
+        max_seq_length: int = None,
+    ) -> None:
+        self.base_name = base_name
+        super().__init__(name, tokenizer, data_path, prompt, max_seq_length)
+
+    def _get_data_from_config(self, config_filename: str, key: str = None):
+        path = os.path.join(self.config_path, config_filename)
+        with open(path, 'r') as f:
+            data = json.load(f)
+
+        if key is not None:
+            return data[key]
+        return data[self.base_name][self.name]
 
 
 class BaseMultiTaskDataset:
@@ -108,7 +267,7 @@ class BaseMultiTaskDataset:
         self,
         dir_path: str,
         tokenizer: PreTrainedTokenizer,
-        prompt: str = "",
+        prompt: str = None,
         max_seq_lenght: int = None,
         name_split_character: str = "-"
     ) -> None:
@@ -134,7 +293,18 @@ class BaseMultiTaskDataset:
                 f"""Bad task name
                 All supported tasks: [{','.join(self.tasks_paths.keys())}]"""
             )
-        return BaseNonLabeledDataset(
+
+        if task_name in GENERATION_TASKS:
+            return InnerTaskGenerationDataset(
+                name=task_name,
+                tokenizer=self.tokenizer,
+                data_path=task_path,
+                prompt=self.prompt,
+                max_seq_length=self.max_seq_length
+            )
+
+        return InnerTaskClassificationDataset(
+            name=task_name,
             tokenizer=self.tokenizer,
             data_path=task_path,
             prompt=self.prompt,
@@ -142,36 +312,35 @@ class BaseMultiTaskDataset:
         )
 
 
-class BaseQADataset(BaseDataset):
+class BaseQADataset(BaseClassificationDataset):
     def __init__(
         self,
+        name: str,
         tokenizer: PreTrainedTokenizer,
         data_path: str,
         prompt: str = "",
         max_seq_length: int = None,
-        labels_set: Sequence[str] = None
     ) -> None:
-        super().__init__(tokenizer, data_path, prompt, max_seq_length)
-        self.labels_set = labels_set
+        super().__init__(name, tokenizer, data_path, prompt, max_seq_length)
 
     def _make_options(self, options: dict) -> str:
         options_list = "\n".join(f"{k}: {v}" for k, v in options.items())
-        return "\nOptions:\n" + options_list + "\n"
+        return "\n\nOPTIONS:\n" + options_list + "\n"
 
     def _process_data(self) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        diagnoses = self.df['input']
+        questions = self.df['input']
         options = self.df['options']
         answers = list(self.df['target'])
         labels = list(self.df['label'])
-        if self.labels_set is None:
-            self.labels_set = set(labels)
-        labels = labels_to_numbers(labels, self.labels_set)
+        num_labels = labels_to_numbers(labels, self.labels)
 
-        messages = [self.prompt
-                    + diagnosis
-                    + self._make_options(cur_options)
-                    + "Response:\n"
-                    for diagnosis, cur_options in zip(diagnoses, options)]
+        inputs = ["\n\nQUESTION:\n"
+                  + question
+                  + self._make_options(cur_options)
+                  for question, cur_options in zip(questions, options)]
+
+        messages = [self.prompt.replace('<INPUT>', input)
+                    for input in inputs]
 
         input_ids, attention_mask = self._tokenize(messages)
         self.answers_ids, _ = self._tokenize(answers)
@@ -179,7 +348,7 @@ class BaseQADataset(BaseDataset):
         return (
             input_ids.type(torch.long),
             attention_mask.type(torch.long),
-            torch.Tensor(labels).type(torch.long)
+            torch.Tensor(num_labels).type(torch.long)
         )
 
     def __getitem__(
