950cc5564f52cfc375596b4e7e7f4fcecdd473ef
zhuravlevvik <viktor.zhuravlev2003@mail.ru>
2025-03-17 18:26:58 +0300

    data downloading script changin

--- scripts/dataset_downloading.sh
+++ scripts/dataset_downloading.sh
@@ -1,3 +1,4 @@
+DATA_PATH=~/autoprompting_data
 PROJECT_ROOT=~/CoolPrompt
 
 while [[ "$#" -gt 0 ]]; do
@@ -14,15 +15,10 @@ if [[ -z "$login" || -z "$password" ]]; then
     exit 1
 fi
 
-DATA_PATH=$PROJECT_ROOT/data
-
 echo "Downloading..."
 python3 $PROJECT_ROOT/src/utils/scripts/dataset_downloading.py --login $login --password $password --save_path $DATA_PATH/datasets.zip
 
 unzip $DATA_PATH/datasets.zip -d $DATA_PATH
-
-rm $DATA_PATH/data/*.json
-rm $DATA_PATH/data/*.py
 
 mv $DATA_PATH/data/* $DATA_PATH
 
