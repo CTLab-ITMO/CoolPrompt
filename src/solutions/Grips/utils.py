3b7cb860f8ba25069b4de606727b10ce995c9409
eric <e.dyagin>
2025-04-10 10:59:30 +0300

    grips full + eval minor change

--- src/solutions/Grips/utils.py
+++ src/solutions/Grips/utils.py
@@ -1,4 +1,5 @@
 from transformers import AutoTokenizer
+from vllm import LLM
 
 def setup_tokenizer(model_name: str) -> AutoTokenizer:
     
@@ -7,3 +8,10 @@ def setup_tokenizer(model_name: str) -> AutoTokenizer:
     tokenizer.pad_token = tokenizer.eos_token
 
     return tokenizer # type: ignore
+
+
+def setup_vllm_model(model_name: str) -> LLM:
+    
+    model = LLM(model=model_name, dtype="float16", trust_remote_code=True)
+
+    return model # type: ignore
