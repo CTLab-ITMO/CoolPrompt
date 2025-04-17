aa5fc56c8210efaaabf3b67cd151a6576716c309
N Kulin <n-kulin>
2025-04-17 20:51:16 +0300

    Добавил первую структуру внутри CoolPrompt

---
+++ coolprompt/assistant.py
@@ -0,0 +1,26 @@
+class PromptHelper:
+    '''Docstring'''
+    def __init__(self):
+        '''Docstring
+
+        Args:
+            model_name: str
+            config: dict
+            token: str = None
+        '''
+        token = ...
+        config = ...
+        model = ...
+        pass
+
+    def invoke(self, ):
+        '''Docstring
+
+        Args:
+            start_prompt: str
+            dataset: pd.DataFrame
+            target: str
+        Returns:
+            final_prompt: str
+        '''
+        pass
---
+++ coolprompt/language_model/llm.py
@@ -0,0 +1,7 @@
+'''List of supported LLM Interface'''
+
+
+class BaseLLM:
+    '''Base LLM interface'''
+    def __init__(self):
+        pass
