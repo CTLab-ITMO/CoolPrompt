cd429dd699164adebcad231c515fbc598e69a528
NikitaKulin <serito1998@gmail.com>
2025-05-25 11:15:13 +0300

    первый коммит для теста

--- src/solutions/evo/base/__init__.py
+++
@@ -1,9 +0,0 @@
-from src.solutions.evo.base.evoluter import Evoluter
-from src.solutions.evo.base.prompt import Prompt, PromptOrigin
-
-
-__all__ = [
-    'Evoluter',
-    'Prompt',
-    'PromptOrigin'
-]
