0c245dcb8ca30c08561b038448f89d0622fed952
zhuravlevvik <viktor.zhuravlev2003@mail.ru>
2025-05-29 18:04:43 +0300

    better export from evaluator

---
+++ coolprompt/evaluator/__init__.py
@@ -0,0 +1,8 @@
+from coolprompt.evaluator.evaluator import Evaluator
+from coolprompt.evaluator.metrics import ClassificationMetric, GenerationMetric
+
+__all__ = [
+    'Evaluator',
+    'ClassificationMetric',
+    'GenerationMetric'
+]
