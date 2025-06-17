4e805c394158cb07fdb36ef576ef4c5852187d71
zhuravlevvik <viktor.zhuravlev2003@mail.ru>
2025-06-17 15:49:37 +0300

    test utils

---
+++ test/coolprompt/utils/parsing.py
@@ -0,0 +1,57 @@
+import sys
+import os
+import unittest
+
+project_root = os.path.abspath(os.path.join(os.getcwd(), "../../../"))
+sys.path.append(project_root)
+
+from coolprompt.utils.parsing import extract_answer
+
+
+class TestExtractAnswer(unittest.TestCase):
+    TAGS = ('<a>', '</a>')
+    FORMAT_MISMATCH_LABEL = -42
+
+    def test_extract_answer_correct(self):
+        result = extract_answer("<a>Answer</a>", self.TAGS)
+        self.assertIsInstance(result, str)
+        self.assertEqual(result, 'Answer')
+
+    def test_extract_answer_incorrect_tag(self):
+        result = extract_answer(
+            "<a>Answer</b>",
+            self.TAGS,
+            self.FORMAT_MISMATCH_LABEL
+        )
+        self.assertIsInstance(result, int)
+        self.assertEqual(result, self.FORMAT_MISMATCH_LABEL)
+
+    def test_extract_answer_no_tags(self):
+        self.assertEqual(
+            extract_answer(
+                "Answer",
+                self.TAGS,
+                self.FORMAT_MISMATCH_LABEL
+            ),
+            self.FORMAT_MISMATCH_LABEL
+        )
+
+    def test_extract_answer_no_opening_tag(self):
+        self.assertEqual(
+            extract_answer(
+                "Answer</a>",
+                self.TAGS,
+                self.FORMAT_MISMATCH_LABEL
+            ),
+            self.FORMAT_MISMATCH_LABEL
+        )
+
+    def test_extract_answer_no_closing_tag(self):
+        self.assertEqual(
+            extract_answer(
+                "<a>Answer<",
+                self.TAGS,
+                self.FORMAT_MISMATCH_LABEL
+            ),
+            self.FORMAT_MISMATCH_LABEL
+        )
---
+++ test/coolprompt/utils/validation.py
@@ -0,0 +1,27 @@
+import sys
+import os
+import unittest
+from unittest.mock import patch, ANY
+
+project_root = os.path.abspath(os.path.join(os.getcwd(), "../../../"))
+sys.path.append(project_root)
+
+from coolprompt.utils.validation import validate_model
+
+
+class TestValidateModel(unittest.TestCase):
+
+    def setUp(self):
+        self.patcher = patch(
+            'coolprompt.utils.validation.isinstance'
+        )
+        self.isinstance_mock = self.patcher.start()
+
+    def test_validate_model_correct(self):
+        self.isinstance_mock.return_value = True
+        validate_model(ANY)
+
+    def test_validate_model_incorrect(self):
+        self.isinstance_mock.return_value = False
+        with self.assertRaises(TypeError):
+            validate_model(ANY)
