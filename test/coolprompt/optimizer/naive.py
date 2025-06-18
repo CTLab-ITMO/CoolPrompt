18487f94b27e19219431aa6b8dabdeea107d29b0
zhuravlevvik <viktor.zhuravlev2003@mail.ru>
2025-06-18 11:59:49 +0300

    test naive optimizer

---
+++ test/coolprompt/optimizer/naive.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import unittest
+from unittest.mock import MagicMock
+
+project_root = os.path.abspath(os.path.join(os.getcwd(), "../../../"))
+sys.path.append(project_root)
+
+from coolprompt.optimizer.naive import naive_optimizer
+
+
+class TestNaiveOptimizer(unittest.TestCase):
+
+    def setUp(self):
+        self.mock_model = MagicMock()
+        self.mock_model.invoke.return_value = \
+            "[PROMPT_START]Final Prompt[PROMPT_END]"
+
+    def test_naive_optimizer(self):
+        self.assertEqual(
+            "Final Prompt",
+            naive_optimizer(self.mock_model, "Input Prompt")
+        )
+        self.mock_model.invoke.assert_called_once()
