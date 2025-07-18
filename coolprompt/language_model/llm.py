42241b2fed2db0d93e353f39a83d4561f441f0f8
Artur Khairullin <368983@niuitmo.ru>
2025-07-19 02:19:24 +0300

    added different langchain providers and default parameters for them

--- coolprompt/language_model/llm.py
+++ coolprompt/language_model/llm.py
@@ -10,8 +10,13 @@ from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
 from langchain_core.language_models.base import BaseLanguageModel
 from coolprompt.utils.logging_config import logger
 from coolprompt.utils.default import (
+    DEFAULT_HF_ENDPOINT_MODEL_NAME,
+    DEFAULT_HF_ENDPOINT_PROVIDER,
+    DEFAULT_HF_MODEL_PARAMETERS,
     DEFAULT_MODEL_NAME,
     DEFAULT_MODEL_PARAMETERS,
+    DEFAULT_OLLAMA_MODEL_NAME,
+    DEFAULT_OUTLINES_MODEL_PARAMETERS,
 )
 
 
@@ -20,6 +25,42 @@ class DefaultLLM:
 
     @staticmethod
     def init(
+        langchain_provider: str = "vllm",
+        **kwargs,
+    ) -> BaseLanguageModel:
+        """Initializing the default LLM via given provider
+
+        Args:
+            langchain_provider (str): provider used for model's initialization.
+            Available providers: `vllm` (default), `hf_endpoint`,
+            `hf_pipeline`, `llamacpp`, `ollama`, `gpt4all`,
+            `outlines`."""
+
+        provider = langchain_provider
+        logger.info(f"Initializing with provider: {provider}")
+        provider_map = {
+            "vllm": DefaultLLM.init_vllm,
+            "hf_endpoint": DefaultLLM.init_hf_endpoint,
+            "hf_pipeline": DefaultLLM.init_hf_pipeline,
+            "llamacpp": DefaultLLM.init_llamacpp,
+            "ollama": DefaultLLM.init_ollama,
+            "gpt4all": DefaultLLM.init_gpt4all,
+            "outlines": DefaultLLM.init_outlines,
+        }
+
+        if provider not in provider_map:
+            error_msg = (
+                f"Invalid provider '{provider}'. Allowed providers: "
+                f"{list(provider_map.keys())}"
+            )
+            logger.error(error_msg)
+            raise ValueError(error_msg)
+
+        logger.debug(f"Itinializing with given kwargs: {kwargs}")
+        return provider_map[provider](**kwargs)
+
+    @staticmethod
+    def init_vllm(
         langchain_config: dict[str, any] | None = None,
         vllm_engine_config: dict[str, any] | None = None,
     ) -> BaseLanguageModel:
