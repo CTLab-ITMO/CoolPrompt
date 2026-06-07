f7d6417c14b82c1a0dc493af528a969cc99822fd
Nikita Kulin <serito1998@gmail.com>
2026-06-07 21:47:09 +0300

    prepare for 2.0.0

--- test/coolprompt/optimizer/hyper/test_package_exports.py
+++
@@ -1,9 +0,0 @@
-def test_hyper_package_exports():
-    import coolprompt.optimizer.hyper as hyper_pkg
-
-    names = set(hyper_pkg.__all__)
-    assert "MetaPromptOptimizer" in names
-    assert "HyPERLightMethod" in names
-    assert "HyPEROptimizer" in names
-    assert "HyPERMethod" in names
-    assert "Optimizer" in names
